7fe381c648a3036edd129cf90777f173c9747e29
simpart <taria.ki4m@gmail.com>
2016-12-16 07:13:13 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * @file   Image.js
+ * @brief  Base UI Image Class
+ * @author simpart
+ */
+
+module.exports = class extends mofron.parts.Base {
+    
+    initContents (vd, prm) {
+        try {
+            if ('string' !== (typeof prm)) {
+                throw new Error('invalid parameter');
+            }
+            var image = new mofron.util.Vdom('img');
+            image.setAttr('src', prm);
+            vd.addChild(image);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+}
