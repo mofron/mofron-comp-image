6de7b8c8681bac3db3d21517eeed08e04f1de6de
simpart <taria.ki4m@gmail.com>
2016-12-17 13:08:49 +0900

    add size

--- index.js
+++ index.js
@@ -4,7 +4,15 @@
  * @author simpart
  */
 
-module.exports = class extends mofron.parts.Base {
+mofron.parts.Image = class extends mofron.parts.Base {
+    getTarget () {
+        try {
+            return this.vdom.getChild(0);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
     
     initContents (vd, prm) {
         try {
@@ -14,6 +22,56 @@ module.exports = class extends mofron.parts.Base {
             var image = new mofron.util.Vdom('img');
             image.setAttr('src', prm);
             vd.addChild(image);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    size (hei, wid) {
+        try {
+            var _hei = (hei === undefined) ? null : hei;
+            var _wid = (wid === undefined) ? null : wid;
+            if ((null === _hei) || (null === _wid)) {
+                return {
+                    this.height(),
+                    this.width()
+                };
+            }
+            this.height(hei);
+            this.width(wid);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    width (val) {
+        try {
+            var _val = (val === undefined) ? null : val;
+            if (null === _val) {
+                return this.getTarget().getAttr('width');
+            }
+            if ('number' !== (typeof _val)) {
+                throw new Error('invalid parameter');
+            }
+            this.getTarget().setAttr('width', val);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    height (val) {
+        try {
+            var _val = (val === undefined) ? null : val;
+            if (null === _val) {
+                return this.getTarget().getAttr('height');
+            }
+            if ('number' !== (typeof _val)) {
+                throw new Error('invalid parameter');
+            }
+            this.getTarget().setAttr('height', val);
         } catch (e) {
             console.error(e.stack);
             throw e;
