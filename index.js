55302c11ad526c6f5735a1239d557098d026c9c4
simpart <taria.ki4m@gmail.com>
2017-08-12 15:13:08 +0900

    add path

--- index.js
+++ index.js
@@ -22,16 +22,35 @@ mofron.comp.Image = class extends mofron.Component {
     
     initDomConts (prm) {
         try {
-            if ('string' !== (typeof prm)) {
-                throw new Error('invalid parameter');
-            }
             this.vdom().addChild(
                 new mofron.Dom({
                     tag       : 'img',
                     component : this,
-                    attr      : { src : prm }
+                    attr      : {
+                        src : (null !== prm) ? prm : ''
+                    }
                 })
             );
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    path (val) {
+        try {
+            if (undefined === val) {
+                /* getter */
+                return (undefined === this.m_path) ? null : this.m_path;
+            }
+            /* setter */
+            if ('string' !== typeof val) {
+                throw new Error('invalid parameter');
+            }
+            this.m_path = val;
+            this.target().attr({
+                src : val
+            });
         } catch (e) {
             console.error(e.stack);
             throw e;
